abd3a61f4573f37de54494841a3e4262d1c373cf
fuselwolga <145371114+fuselwolga@users.noreply.github.com>
2023-10-07 03:21:53 +0200

    Update Start.py

--- Start.py
+++ Start.py
@@ -16,7 +16,7 @@ st.markdown(
     Sämtliche genutzte Daten kommen von [fbref.com](https://fbref.com/en/comps/33/2-Bundesliga-Stats/).
     In der Liste links kannst du zwischen verschiedenen Punkten auswählen - von einfachen Übersichten bis
     zu interaktiven Darstellungen.\\
-    Die App wurde mithilfe des Python-Pakets *Streamlit* erstellt. Optimiert ist sie für die Nutzung im Browser an einem Computer.
+    Die App wurde mithilfe des Python-Pakets [*Streamlit*](https.streamlit.io) erstellt. Optimiert ist sie für die Nutzung im Browser an einem Computer.
     Sie funktioniert auch auf dem Smartphone, manche Darstellungen sind unter Umständen nicht optimal. 
     An einigen Stellen kann über einen Regler die Smartphone-Version aktiviert werden.
     """
