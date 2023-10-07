68061ff6f9508c59a5c37855146253dd92ac57e9
fuselwolga <145371114+fuselwolga@users.noreply.github.com>
2023-10-07 03:21:28 +0200

    Update 2_Spieltag.py

--- pages/2_Spieltag.py
+++ pages/2_Spieltag.py
@@ -130,7 +130,7 @@ for index, row in md.iterrows():
 st.subheader("Spieltage",divider = "rainbow")
 Start_index = len(mdSubset[(mdSubset["Heim"] == "Hannover 96")|(mdSubset["Auswärts"] == "Hannover 96")]) # aktuelle Anzahl von Spielen
 on = st.toggle("Smartphone-Version",key = "md-toogle_mobile")
-Spieltag = st.selectbox("",options = range(1,35),index=Start_index)
+Spieltag = st.selectbox("",options = range(1,35),index=Start_index-1)
 if on:
    st.write(f"__Spieltag {Spieltag}: {md[md['Spieltag'] == Spieltag].reset_index().loc[0,'Datum']} - {md[md['Spieltag'] == Spieltag].reset_index().loc[7,'Datum']}__")
    st.table(md_small[md_small["Spieltag"] == Spieltag].style.set_table_styles(md_small_style))        
