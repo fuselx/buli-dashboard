011dc8af7d321fb5e0870c75019197ea8ccb2cdf
fuselwolga <145371114+fuselwolga@users.noreply.github.com>
2023-11-10 12:10:19 +0100

    Update 🏠_Start.py

--- 🏠_Start.py
+++ 🏠_Start.py
@@ -14,15 +14,20 @@ with col2:
     
     st.markdown(
         """
-        In dieser App finden sich unterschiedlichste Statistiken zur zweiten Fußball-Bundesliga. Das Ganze ist ein großes Übungs- und Spaßprojekt. 
-        Sämtliche genutzte Daten kommen von [fbref.com](https://fbref.com/en/comps/33/2-Bundesliga-Stats/) und sind mit einer Verzögerung von ca. einem Tag aktuell.
-        In der Liste links kannst du zwischen verschiedenen Punkten auswählen - von einfachen Übersichten bis
-        zu interaktiven Darstellungen.\\
-        Die App wurde mithilfe des Python-Pakets [*Streamlit*](https.streamlit.io) erstellt. Optimiert ist sie für die Nutzung im Browser.
-        Sie funktioniert auch auf dem Smartphone, manche Darstellungen sind jedoch unter Umständen nicht optimal. 
-        Im Menü links kann die Smartphone-Version aktiviert werden.
+        In dieser App finden sich unterschiedlichste Informationen zur zweiten Fußball-Bundesliga. Zwischen diesen kannst du in der Leiste links auswählen.
+        Neben der Tabelle und Spielplänen findest du aufbereitete Daten und Statistiken, mit denen die Leistungen und Spielweisen der Teams 
+        analysiert werden können. 
         """
         )
     
     st.image("https://raw.githubusercontent.com/fuselwolga/buli-dashboard/main/Aufstieg_2002.jpg")
     st.caption("Symbolbild")
+    with st.expander("Weitere Infos"):
+        st.markdown(
+            """
+            Das Ganze ist ein großes Übungs- und Spaßprojekt. 
+            Sämtliche genutzte Daten kommen von [fbref.com](https://fbref.com/en/comps/33/2-Bundesliga-Stats/) und sind mit einer Verzögerung von ca. einem Tag aktuell.\\
+            Die App wurde mithilfe des Python-Pakets [*Streamlit*](https.streamlit.io) erstellt. Optimiert ist sie für die Nutzung im Browser an einem Computer.
+            Sie funktioniert auch auf dem Smartphone, manche Darstellungen sind jedoch unter Umständen nicht optimal. 
+            In der Leiste links kann die Smartphone-Version aktiviert werden. Diese entschlackt einige Tabellen, so dass Betrachtung und Bedienung auf mobilen Geräten einfacher ist.
+            """)
